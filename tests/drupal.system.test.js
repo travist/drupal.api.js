98ca1c65a3f06261fe8fcfd2d4e398151b1187d1
Travis Tidwell <travis@allplayers.com>
2011-11-15 15:20:00 +0100

    Added a variable_get, variable_set, and variable_del unit test

--- tests/drupal.system.test.js
+++ tests/drupal.system.test.js
@@ -7,3 +7,31 @@ asyncTest("System Connect", function() {
     ok(!!system.user.id, "User is logged in");
   });
 });
+
+asyncTest("Variable Set, Get, Delete", function() {
+  var system = new drupal.system();
+
+  // Set a test variable.
+  system.set_variable('test_variable', '12345', function() {
+
+    // Provide a different default to validate value.
+    system.get_variable('test_variable', '2468', function(value) {
+
+      // Test to make sure the variable was set...
+      var test1 = (value == '12345');
+
+      // Now delete the variable.
+      system.del_variable('test_variable', function() {
+
+        // Now check to make sure it no longer exists.
+        system.get_variable('test_variable', '2468', function(check) {
+
+          start();
+          expect(2);
+          ok(test1, "set_variable & get_variable passed.");
+          ok(check=='2468', "del_variable passed.");
+        });
+      });
+    });
+  });
+});
