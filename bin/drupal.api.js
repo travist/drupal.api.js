01b8af3b181740b3bf0a9b5d7644d1ec9ddd4038
Travis Tidwell <travis@allplayers.com>
2012-07-17 12:49:56 -0500

    Fixed a variable misdefinition.

--- bin/drupal.api.js
+++ bin/drupal.api.js
@@ -727,7 +727,7 @@ drupal.api = function() {
       // See if we should cache the result.
       if (cache) {
         this.cacheId = this.cacheId || url.replace(/[^A-z0-9]/g, '');
-        var storage = drupal.retrieve(cacheId);
+        var storage = drupal.retrieve(this.cacheId);
         if (storage) {
           callback(storage);
           return;
