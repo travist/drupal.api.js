c625f7ebd1894fb255d180c4ca5efcb1b0bc080a
Travis Tidwell <travist349@gmail.com>
2012-11-06 14:42:06 -0600

    Added the less specific caching back in place.

--- bin/drupal.api.js
+++ bin/drupal.api.js
@@ -721,12 +721,13 @@ drupal.api = function() {
       // Get the url for this object.
       var url = this.getURL(object);
       url += (endpoint) ? ('/' + endpoint) : '';
+      var cacheId = url;
       url += '.jsonp';
       url += query ? ('?' + decodeURIComponent(jQuery.param(query, true))) : '';
 
       // See if we should cache the result.
       if (cache) {
-        this.cacheId = url.replace(/[^A-z0-9\-]/g, '');
+        this.cacheId = cacheId.replace(/[^A-z0-9\-]/g, '');
         var storage = drupal.retrieve(this.cacheId);
         if (storage && (storage.url === url)) {
           callback(storage.data);
